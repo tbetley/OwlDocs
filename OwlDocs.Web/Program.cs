c003d434e24017f573860a4c5c67d34259c03474
Tyler Betley <Tyler@betley.io>
2022-01-17 17:33:30 -0600

    Remove Startup.cs, convert to using WebApplication host.

--- OwlDocs.Web/Program.cs
+++ OwlDocs.Web/Program.cs
@@ -13,6 +13,17 @@ using Serilog.Events;
 using OwlDocs.Data;
 using Microsoft.Extensions.DependencyInjection;
 using System.IO;
+using Microsoft.AspNetCore.Builder;
+
+using OwlDocs.Web.Options;
+using OwlDocs.Models.Options;
+
+using Markdig;
+using Microsoft.EntityFrameworkCore;
+using OwlDocs.Domain.Docs;
+using Microsoft.AspNetCore.Server.IISIntegration;
+using OwlDocs.Web.Authorization;
+using Microsoft.AspNetCore.Authorization;
 
 namespace OwlDocs.Web
 {
@@ -20,6 +31,7 @@ namespace OwlDocs.Web
     {
         public static void Main(string[] args)
         {
+            // Set up early logging to catch if Host fails to build/run
             var configuration = new ConfigurationBuilder()
                     .SetBasePath(Directory.GetCurrentDirectory())
                     .AddJsonFile("appsettings.json")
@@ -31,13 +43,88 @@ namespace OwlDocs.Web
                     .CreateLogger();
 
             try
-            {
-                var host = CreateHostBuilder(args).Build();
+            {                
+                var builder = WebApplication.CreateBuilder(args);
 
-                SeedDatabase(host);
+                // Add Options 
+                builder.Services.AddOptions<AuthOptions>().Bind(configuration.GetSection(AuthOptions.Authorization));
+                builder.Services.AddOptions<DocumentOptions>().Bind(configuration.GetSection(DocumentOptions.DocumentSettings));
+                builder.Services.AddOptions<SiteOptions>().Bind(configuration.GetSection(SiteOptions.SiteSettings));
 
-                host.Run();
+                // Add 3rd party services
+                builder.Services.AddSingleton(md => new MarkdownPipelineBuilder().UseAdvancedExtensions().Build());
+                
+                var docProvider = configuration["DocumentSettings:Provider"];
+                if (docProvider == "Database")
+                {
+                    // add database services
+                    builder.Services.AddDbContext<OwlDocsContext>(options =>
+                    {
+                        options.UseSqlServer(configuration.GetConnectionString("TestConnection"));
+                    });
 
+                    builder.Services.AddScoped<IDocumentService, DbDocumentService>();
+                }
+                else if (docProvider == "File")
+                {
+                    // add file services
+                    builder.Services.AddScoped<IDocumentService, FileDocumentService>();
+                }
+
+                builder.Services.AddSingleton<IDocumentCache, DocumentCache>();
+
+
+                if (configuration.GetValue<string>("Authoization:Type") == AuthorizationType.ActiveDirectory.ToString("D"))
+                {
+                    builder.Services.AddAuthentication(IISDefaults.AuthenticationScheme);
+                }
+
+                builder.Services.AddAuthorization(options =>
+                {
+                    options.AddPolicy(Policies.DocumentReadersPolicy,
+                        policy => policy.Requirements.Add(new DocumentReadersRequirement()));
+
+                    options.AddPolicy(Policies.DocumentWritersPolicy,
+                        policy => policy.Requirements.Add(new DocumentWritersRequirement()));
+
+                    options.AddPolicy(Policies.SiteAdminsPolicy,
+                        policy => policy.Requirements.Add(new SiteAdminsRequirement()));
+
+                });
+
+                builder.Services.AddSingleton<IAuthorizationHandler, DocumentReadersHandler>();
+                builder.Services.AddSingleton<IAuthorizationHandler, DocumentWritersHandler>();
+                builder.Services.AddSingleton<IAuthorizationHandler, SiteAdminsHandler>();
+
+                // Add asp.net core required services
+                builder.Services.AddMvc();
+
+                builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+                var app = builder.Build();               
+
+                var cache = app.Services.GetService<IDocumentCache>();
+
+                using var scope = app.Services.CreateScope();
+                var docSvc = scope.ServiceProvider.GetService<IDocumentService>();
+
+                cache.Tree = docSvc.GetDocumentTree().Result;
+
+                app.UseExceptionHandler("/Error");
+
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+                app.UseRouting();
+                app.UseAuthorization();
+                app.UseAuthentication();
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+
+                SeedDatabase(app);
+
+                app.Run();
             }
             catch (Exception e)
             {
@@ -49,15 +136,6 @@ namespace OwlDocs.Web
             }
 
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-
 
         private static void SeedDatabase(IHost host)
         {
